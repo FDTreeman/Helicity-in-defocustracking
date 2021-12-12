0d466934fc06acfa60fecee60dec3c863964208e
FDTreeman <46095762+FDTreeman@users.noreply.github.com>
2021-12-12 11:08:03 +0800

    Update GDPT_import_data.py

--- GDPT_import_data.py
+++ GDPT_import_data.py
@@ -12,98 +12,30 @@ bins = 30
 
 #import data
 
-def folder(l,exp,filepath):
+def folder(l,filepath):
     '''
-    l = [-1,-5,-10,-15,-20]
-    exp = [1,2,3]
+    Here are providing two examples: w-1 and w-15, 
+    so,l = [-1,-15]
     filepath: the data filepath
     
     '''
     if l == -1:
-        if exp == 1:
-            filename = 'w1_exp1'
-            eps = 0.9
-        elif exp == 2:
-            filename = 'w1_exp2'
-            eps = 0.92 # or 0.93
-        elif exp == 3:
-            filename = 'w1_exp3'
-            eps = 0.8
-        else:
-            raise Exception("Invalid level!")    
+        filename = 'w1'
+        eps = 0.92 # or 0.93
         folder = filepath + filename
-        
-        
-    elif l == -5:
-        if exp == 1:
-            filename = 'w5_exp1'
-            eps = 0.31
-        elif exp == 2:
-            filename = 'w5_exp2'
-            eps = 0.1
-        elif exp == 3:
-            filename = 'w5_exp3'
-            eps = 0.66
-        else:
-            raise Exception("Invalid level!")
-        folder = filepath + filename
-        
-        
-    elif l == -10:
-    
-        if exp == 1:
-            filename = 'w10_exp1'
-            eps = 0.41
-        elif exp == 2:
-            filename = 'w10_exp2'
-            eps = 0.3
-        elif exp == 3:
-            filename = 'w10_exp3'
-            eps = 0.4
-        else:
-            raise Exception("Invalid level!")   
-        folder = filepath + filename
-        
-        
     elif l == -15:
-        if exp == 1:
-            filename = 'w15_exp1'
-            eps = 0.31
-        elif exp == 2:
-            filename = 'w15_exp2'
-            eps = 0.3
-        elif exp == 3:
-            filename = 'w15_exp3'
-            eps = 0.31
-        else:
-            raise Exception("Invalid level!")  
+        filename = 'w15'
+        eps = 0.31
         folder = filepath + filename
-        
-        
-    elif l == -20:
-        if exp == 1:
-            filename = 'w20_exp1'
-            eps = 0.31
-        elif exp == 2:
-            filename = 'w20_exp2'
-            eps = 0.3
-        elif exp == 3:
-            filename = 'w20_exp3'
-            eps = 0.26
-        else:
-            raise Exception("Invalid level!")
-        folder = filepath + filename
-        
-        
     else:
         raise Exception("Invalid level!")
 
-    return folder,eps
+    return folder,filename,eps
 
 
 def import_data(folder):
     '''
-    
+    read data
     
     '''
     files = ['Cm','DX','DY','DZ','ID','In','X','Y','Z']
